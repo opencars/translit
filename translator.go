a8f6980da9e15f0f38e1d5ff6433d63bc5365df5
Ali Shanaakh <github@shal.dev>
2019-12-23 21:10:29 +0200

    Feature: Ability to transliterate from cyrillic to latin

--- translator.go
+++ translator.go
@@ -32,12 +32,55 @@ func RuneToUA(char rune) rune {
 	}
 }
 
-// ToUA translates string in Ukrainian to Latin equivalent.
+// RuneToUA returns Ukrainian equivalent of Latin character.
+func RuneToLatin(char rune) rune {
+	switch char {
+	case 'А':
+		return 'A'
+	case 'В':
+		return 'B'
+	case 'С':
+		return 'C'
+	case 'Е':
+		return 'E'
+	case 'Н':
+		return 'H'
+	case 'І':
+		return 'I'
+	case 'К':
+		return 'K'
+	case 'М':
+		return 'M'
+	case 'О':
+		return 'O'
+	case 'Р':
+		return 'P'
+	case 'Т':
+		return 'T'
+	case 'Х':
+		return 'X'
+	default:
+		return char
+	}
+}
+
+// ToUA translates string from latin to cyrillic equivalent.
 func ToUA(lexeme string) string {
 	chars := make([]rune, 0)
 
 	for _, v := range lexeme {
 		chars = append(chars, RuneToUA(v))
+	}
+
+	return string(chars)
+}
+
+// ToLatin( translates string from cyrillic to latin equivalent.
+func ToLatin(lexeme string) string {
+	chars := make([]rune, 0)
+
+	for _, v := range lexeme {
+		chars = append(chars, RuneToLatin(v))
 	}
 
 	return string(chars)
--- translator_test.go
+++ translator_test.go
@@ -16,6 +16,14 @@ func TestRuneToUA(t *testing.T) {
 	assert.Equal(t, '1', RuneToUA('1'))
 }
 
+func TestRuneToLatin(t *testing.T) {
+	assert.Equal(t, 'A', RuneToLatin('А'))
+	assert.Equal(t, 'B', RuneToLatin('В'))
+	assert.Equal(t, 'C', RuneToLatin('С'))
+	assert.Equal(t, 'Z', RuneToLatin('Z'))
+	assert.Equal(t, '1', RuneToLatin('1'))
+}
+
 func TestToUA(t *testing.T) {
 	t.Run("simple strings", func(t *testing.T) {
 		assert.Equal(t, "АВС", ToUA("ABC"))
@@ -47,6 +55,37 @@ func TestToUA(t *testing.T) {
 	})
 }
 
+func TestToLatin(t *testing.T) {
+	t.Run("simple strings", func(t *testing.T) {
+		assert.Equal(t, "ABC", ToLatin("АВС"))
+		assert.Equal(t, "AA0000AA", ToLatin("АА0000АА"))
+	})
+
+	t.Run("without cyrillic", func(t *testing.T) {
+		assert.Equal(t, "ABC", ToLatin("ABC"))
+		assert.Equal(t, "123456789", ToLatin("123456789"))
+		assert.Equal(t, "AX1234BA", ToLatin("AX1234BA"))
+	})
+
+	t.Run("cyrillic to latin for each region", func(t *testing.T) {
+		actual := []string{
+			"АК", "АВ", "АС", "АЕ", "АН", "АМ", "АО", "АР", "АТ",
+			"АА", "АІ", "ВА", "ВВ", "ВС", "ВЕ", "ВН", "ВІ", "ВК",
+			"СН", "ВМ", "ВО", "АХ", "ВТ", "ВХ", "СА", "СВ", "СЕ",
+		}
+
+		expected := []string{
+			"AK", "AB", "AC", "AE", "AH", "AM", "AO", "AP", "AT",
+			"AA", "AI", "BA", "BB", "BC", "BE", "BH", "BI", "BK",
+			"CH", "BM", "BO", "AX", "BT", "BX", "CA", "CB", "CE",
+		}
+
+		for i := range actual {
+			assert.Equal(t, expected[i], ToLatin(actual[i]))
+		}
+	})
+}
+
 func BenchmarkToUA(b *testing.B) {
 	fixtures := make([]string, b.N)
 
@@ -73,5 +112,34 @@ func BenchmarkRuneToUA(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		RuneToUA(fixtures[i])
+	}
+}
+
+func BenchmarkToLatin(b *testing.B) {
+	fixtures := make([]string, b.N)
+
+	for i := 0; i < b.N; i++ {
+		fixtures[i] = strconv.Itoa(1000 + rand.Int()%8999)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		ToLatin("АА" + fixtures[i] + "ВР")
+	}
+}
+
+func BenchmarkRuneToLatin(b *testing.B) {
+	fixtures := make([]rune, b.N)
+
+	for i := 0; i < b.N; i++ {
+		fixtures[i] = rune('А' + rand.Int()%'Я')
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		RuneToLatin(fixtures[i])
 	}
 }
